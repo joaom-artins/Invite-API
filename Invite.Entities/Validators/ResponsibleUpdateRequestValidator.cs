fe0bf627cc5c7329203555adefd58da64d52c4d9
Joao Martins <milanezijoao51@gmail.com>
2024-07-05 12:38:52 -0300

    feat: Cria validators das requests

---
+++ Invite.Entities/Validators/ResponsibleUpdateRequestValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using Invite.Entities.Requests;
+
+namespace Invite.Entities.Validators;
+
+public class ResponsibleUpdateRequestValidator : AbstractValidator<ResponsibleUpdateRequest>
+{
+    public ResponsibleUpdateRequestValidator()
+    {
+        RuleFor(x => x.Name)
+           .NotEmpty().WithMessage("Nome é um campo obrigatório!")
+           .MinimumLength(3).WithMessage("Nome deve ter no mínimo 3 caracteres")
+           .MaximumLength(60).WithMessage("Nome deve ter no máximo 60 caractéres");
+
+        RuleFor(x => x.CPF)
+            .NotEmpty().WithMessage("CPF é um campo obrigatório!")
+            .MaximumLength(14).WithMessage("CPF deve ter no máximo 14 caractéres");
+
+        RuleFor(x => x.PersonInFamily)
+            .NotEmpty().WithMessage("Pessoas na família é um campo obrigatório!");
+    }
+}
