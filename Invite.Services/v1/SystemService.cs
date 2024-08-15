f10fc280bc073eedb729af4d3b59ab6773a677c1
Joao Martins <milanezijoao51@gmail.com>
2024-08-14 21:14:10 -0300

    finaliza estrutura do metodo

--- Invite.Services/v1/SystemService.cs
+++ Invite.Services/v1/SystemService.cs
@@ -1,8 +1,44 @@
+using Invite.Commons;
+using Invite.Commons.Notifications.Interfaces;
+using Invite.Persistence.Repositories.Interfaces.v1;
+using Invite.Persistence.UnitOfWorks.Interfaces;
 using Invite.Services.Interfaces.v1;
 
 namespace Invite.Services.v1;
 
-public class SystemService : ISystemService
+public class SystemService(
+    AppSettings _appSettings,
+    INotificationContext _notificationContext,
+    IUnitOfWork _unitOfWork,
+    IUserRepository _userRepository,
+    IInvoiceService _invoiceService
+) : ISystemService
 {
-    
+    public async Task<bool> AutomatedInvoiceCreateasync()
+    {
+        var users = await _userRepository.FindByDueDayAsync(DateTime.Now.AddDays(_appSettings.Invoice.DaysBeforeCreate).Day);
+        if (!users.Any())
+        {
+            return false;
+        }
+
+        foreach (var user in users)
+        {
+            try
+            {
+                _unitOfWork.BeginTransaction();
+                await _invoiceService.CreateAsync(user.Id);
+                if (_notificationContext.HasNotifications)
+                {
+                    continue;
+                }
+            }
+            catch (Exception)
+            {
+                _unitOfWork.Rollback();
+            }
+        }
+
+        return true;
+    }
 }
