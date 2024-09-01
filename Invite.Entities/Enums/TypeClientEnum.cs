49321a36ff137d665935ea8fee15c91c0f70087c
Joao Martins <milanezijoao51@gmail.com>
2024-09-01 18:33:37 -0300

    fix: Remove tipo de plano

--- Invite.Entities/Enums/TypeClientEnum.cs
+++
@@ -1,7 +0,0 @@
-namespace Invite.Entities.Enums;
-
-public enum ClientTypeEnum
-{
-    Buffet = 1,
-    Person = 2
-}
--- Invite.Entities/Validators/PlanCreateRequestValidator.cs
+++ Invite.Entities/Validators/PlanCreateRequestValidator.cs
@@ -12,10 +12,6 @@ public class PlanCreateRequestValidator : AbstractValidator<PlanCreateRequest>
              .MinimumLength(5).WithMessage("Plano deve ter no mínimo 5 caracteres!")
              .MaximumLength(40).WithMessage("Plano deve ter no máximo 60 caracteres");
 
-        RuleFor(x => x.Type)
-            .NotEmpty().WithMessage("Tipo de plano é um campo obrigatório!")
-            .IsInEnum().WithMessage("Tipo inválido!");
-
         RuleFor(x => x.Price)
             .NotEmpty().WithMessage("Preço é um campo obrigatório!");
 
